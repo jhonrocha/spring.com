9366851f3a796ef430545e2cefdf62eca60c7b8b
Jhon Rocha <jhmrocha@gmail.com>
2023-09-30 11:51:34 -0300

    Starting Spring Application

---
+++ src/main/java/com/spring/spatfct/SpatfctApplication.java
@@ -0,0 +1,13 @@
+package com.spring.spatfct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpatfctApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpatfctApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/spatfct/SpatfctApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.spatfct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpatfctApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
